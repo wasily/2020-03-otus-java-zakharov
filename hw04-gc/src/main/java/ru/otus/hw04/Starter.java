2bc00fb758a6928fa4fc6dd6be04deb4044212c6
wasily <zaharov_vs@mail.ru>
2020-04-28 18:49:28 +0300

    fix

--- hw04-gc/src/main/java/ru/otus/hw04/Starter.java
+++ hw04-gc/src/main/java/ru/otus/hw04/Starter.java
@@ -12,6 +12,10 @@ Shenandoah:
 -Xmx250m -Xms250m -Xlog:gc -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -Xlog:gc=debug:file=./logs/gc-%p-%t.log:tags,uptime,time,level -XX:+PrintCommandLineFlags
 -Xmx7500m -Xms7500m -Xlog:gc -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -Xlog:gc=debug:file=./logs/gc-%p-%t.log:tags,uptime,time,level -XX:+PrintCommandLineFlags
 
+Parallel:
+-Xmx250m -Xms250m -Xlog:gc -XX:+UseParallelGC -Xlog:gc=debug:file=./logs/gc-%p-%t.log:tags,uptime,time,level -XX:+PrintCommandLineFlags
+-Xmx7500m -Xms7500m -Xlog:gc -XX:+UseParallelGC -Xlog:gc=debug:file=./logs/gc-%p-%t.log:tags,uptime,time,level -XX:+PrintCommandLineFlags
+
 ZGC:
 -Xmx250m -Xms250m -Xlog:gc -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -Xlog:gc=debug:file=./logs/gc-%p-%t.log:tags,uptime,time,level -XX:+PrintCommandLineFlags
 -Xmx7500m -Xms7500m -Xlog:gc -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -Xlog:gc=debug:file=./logs/gc-%p-%t.log:tags,uptime,time,level -XX:+PrintCommandLineFlags
@@ -20,11 +24,9 @@ ZGC:
 public class Starter {
     public static void main(String[] args) {
         //small heap
-        new Demo(1_000_000, 1_500_000).start();
-        new Demo(1_300_000, 2_000_000).start();
+//        new Demo(1_300_000, 2_000_000).start();
 
         //large heap
-        new Demo(30_000_000, 45_000_000).start();
         new Demo(40_000_000, 60_000_000).start();
     }
 }
