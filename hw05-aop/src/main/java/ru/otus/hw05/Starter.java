42645748286783172594fef4c0eba2d1de3eb95c
wasily <zaharov_vs@mail.ru>
2020-05-04 12:59:56 +0300

    some work

--- hw05-aop/src/main/java/ru/otus/hw05/Starter.java
+++ hw05-aop/src/main/java/ru/otus/hw05/Starter.java
@@ -1,12 +1,18 @@
 package ru.otus.hw05;
 
+import ru.otus.hw05.example.Context;
 import ru.otus.hw05.example.ExampleService;
-import ru.otus.hw05.example.ExampleServiceImpl;
 
 public class Starter {
     public static void main(String[] args) {
-        ExampleService service = new ExampleServiceImpl();
-        service.sendMessage("test", false);
-        service.sendMessage("test", true);
+        ExampleService service = Context.getExampleService();
+        service.printCurrentTimeStamp();
+        service.sendMessage("not urgent message", false);
+        service.printCurrentTimeStamp();
+        service.sendMessage("urgent message", true);
+        service.printCurrentTimeStamp();
+        service.sendMessage("repeated message", 3);
+        service.printCurrentTimeStamp();
+        service.sendMessage("regular message");
     }
 }
--- hw05-aop/src/main/java/ru/otus/hw05/example/ExampleService.java
+++ hw05-aop/src/main/java/ru/otus/hw05/example/ExampleService.java
@@ -2,4 +2,7 @@ package ru.otus.hw05.example;
 
 public interface ExampleService {
     void sendMessage(String message, boolean isUrgent);
+    void sendMessage(String message, int times);
+    void sendMessage(String message);
+    void printCurrentTimeStamp();
 }
