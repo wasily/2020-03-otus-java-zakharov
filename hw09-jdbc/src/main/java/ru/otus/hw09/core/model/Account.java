897295d19dc4648a82aa09c2090a163bc93b8d44
wasily <zaharov_vs@mail.ru>
2020-06-11 13:23:25 +0300

    account added

---
+++ hw09-jdbc/src/main/java/ru/otus/hw09/core/model/Account.java
@@ -0,0 +1,42 @@
+package ru.otus.hw09.core.model;
+
+import ru.otus.hw09.jdbc.mapper.annotations.Id;
+
+import java.math.BigDecimal;
+
+public class Account {
+    @Id
+    private long no;
+    private String type;
+    private BigDecimal rest;
+
+    public Account() {
+    }
+
+    public Account(long no, String type, BigDecimal rest) {
+        this.no = no;
+        this.type = type;
+        this.rest = rest;
+    }
+
+    public long getNo() {
+        return no;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public BigDecimal getRest() {
+        return rest;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "no=" + no +
+                ", type='" + type + '\'' +
+                ", rest=" + rest +
+                '}';
+    }
+}
