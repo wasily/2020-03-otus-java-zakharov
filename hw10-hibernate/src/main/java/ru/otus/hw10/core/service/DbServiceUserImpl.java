be56fd852973ef7450650748cdc41d7c42757075
wasily <zaharov_vs@mail.ru>
2020-06-14 17:45:46 +0300

    import ServiceUser

---
+++ hw10-hibernate/src/main/java/ru/otus/hw10/core/service/DbServiceUserImpl.java
@@ -0,0 +1,54 @@
+package ru.otus.hw10.core.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.hw10.core.dao.UserDao;
+import ru.otus.hw10.core.model.User;
+
+import java.util.Optional;
+
+public class DbServiceUserImpl implements DBServiceUser {
+    private static final Logger logger = LoggerFactory.getLogger(DbServiceUserImpl.class);
+
+    private final UserDao userDao;
+
+    public DbServiceUserImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public long saveUser(User user) {
+        try (var sessionManager = userDao.getSessionManager()) {
+            sessionManager.beginSession();
+            try {
+                userDao.insertOrUpdate(user);
+                var userId = user.getId();
+                sessionManager.commitSession();
+
+                logger.info("created/updated user: {}", userId);
+                return userId;
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+                sessionManager.rollbackSession();
+                throw new DbServiceException(e);
+            }
+        }
+    }
+
+    @Override
+    public Optional<User> getUser(long id) {
+        try (var sessionManager = userDao.getSessionManager()) {
+            sessionManager.beginSession();
+            try {
+                Optional<User> userOptional = userDao.findById(id);
+
+                logger.info("user: {}", userOptional.orElse(null));
+                return userOptional;
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+                sessionManager.rollbackSession();
+            }
+            return Optional.empty();
+        }
+    }
+}
