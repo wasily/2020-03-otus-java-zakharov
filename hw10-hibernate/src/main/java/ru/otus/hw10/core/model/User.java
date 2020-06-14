0d77e36b08b0abd22961043831369950f0d6a4b4
wasily <zaharov_vs@mail.ru>
2020-06-14 17:43:51 +0300

    import User

---
+++ hw10-hibernate/src/main/java/ru/otus/hw10/core/model/User.java
@@ -0,0 +1,46 @@
+package ru.otus.hw10.core.model;
+
+import ru.otus.hw10.jdbc.mapper.annotations.Id;
+
+/**
+ * @author sergey
+ * created on 03.02.19.
+ */
+public class User {
+    @Id
+    private long id;
+    private String name;
+
+    public User() {
+    }
+
+    public User(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
