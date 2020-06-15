1faf067994bc0665218cdfd36bb7928f1b65f827
wasily <zaharov_vs@mail.ru>
2020-06-15 19:43:25 +0300

    sample starter

--- hw10-hibernate/src/main/java/ru/otus/hw10/Starter.java
+++ hw10-hibernate/src/main/java/ru/otus/hw10/Starter.java
@@ -2,6 +2,8 @@ package ru.otus.hw10;
 
 import org.hibernate.SessionFactory;
 import ru.otus.hw10.core.dao.UserDao;
+import ru.otus.hw10.core.model.AddressDataSet;
+import ru.otus.hw10.core.model.PhoneDataSet;
 import ru.otus.hw10.core.model.User;
 import ru.otus.hw10.core.service.DBServiceUser;
 import ru.otus.hw10.core.service.DbServiceUserImpl;
@@ -9,15 +11,22 @@ import ru.otus.hw10.hibernate.HibernateUtils;
 import ru.otus.hw10.hibernate.dao.UserDaoHibernate;
 import ru.otus.hw10.hibernate.sessionmanager.SessionManagerHibernate;
 
+import java.util.Set;
+import java.util.UUID;
+
 public class Starter {
     public static void main(String[] args) {
-        SessionFactory sessionFactory = HibernateUtils.buildSessionFactory("hibernate.cfg.xml", User.class);
+        SessionFactory sessionFactory = HibernateUtils.buildSessionFactory("hibernate.cfg.xml",
+                User.class, AddressDataSet.class, PhoneDataSet.class);
 
         SessionManagerHibernate sessionManager = new SessionManagerHibernate(sessionFactory);
         UserDao userDao = new UserDaoHibernate(sessionManager);
         DBServiceUser dbServiceUser = new DbServiceUserImpl(userDao);
 
-        long id = dbServiceUser.saveUser(new User(0, "username"));
+        var address = new AddressDataSet(UUID.randomUUID().toString(),"some address");
+        var phone = new PhoneDataSet(UUID.randomUUID().toString(), "1234567890");
+        User user = new User(0, "username", Set.of(phone),address);
+        long id = dbServiceUser.saveUser(user);
         dbServiceUser.getUser(id);
     }
 }
