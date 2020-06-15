0fd0379987d38311a633652c9363316636957f36
wasily <zaharov_vs@mail.ru>
2020-06-15 15:43:50 +0300

    sample app

---
+++ hw10-hibernate/src/main/java/ru/otus/hw10/Starter.java
@@ -0,0 +1,26 @@
+package ru.otus.hw10;
+
+import org.hibernate.SessionFactory;
+import ru.otus.hw10.core.dao.UserDao;
+import ru.otus.hw10.core.model.User;
+import ru.otus.hw10.core.service.DBServiceUser;
+import ru.otus.hw10.core.service.DbServiceUserImpl;
+import ru.otus.hw10.hibernate.HibernateUtils;
+import ru.otus.hw10.hibernate.dao.UserDaoHibernate;
+import ru.otus.hw10.hibernate.sessionmanager.SessionManagerHibernate;
+
+import java.util.Optional;
+
+public class Starter {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = HibernateUtils.buildSessionFactory("hibernate.cfg.xml", User.class);
+
+        SessionManagerHibernate sessionManager = new SessionManagerHibernate(sessionFactory);
+        UserDao userDao = new UserDaoHibernate(sessionManager);
+        DBServiceUser dbServiceUser = new DbServiceUserImpl(userDao);
+
+        long id = dbServiceUser.saveUser(new User(0, "username"));
+        Optional<User> mayBeCreatedUser = dbServiceUser.getUser(id);
+        System.out.println(mayBeCreatedUser.orElse(null));
+    }
+}
