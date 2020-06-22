9bcb3aed91033521f9c5cd2ea6cb8a92aac8733d
wasily <zaharov_vs@mail.ru>
2020-06-22 10:33:09 +0300

    cachehw imported

---
+++ hw11-cache/src/main/java/ru/otus/hw11/cachehw/MyCache.java
@@ -0,0 +1,33 @@
+package ru.otus.hw11.cachehw;
+
+/**
+ * @author sergey
+ * created on 14.12.18.
+ */
+public class MyCache<K, V> implements HwCache<K, V> {
+
+    @Override
+    public void put(K key, V value) {
+
+    }
+
+    @Override
+    public void remove(K key) {
+
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public void addListener(HwListener<K, V> listener) {
+
+    }
+
+    @Override
+    public void removeListener(HwListener<K, V> listener) {
+
+    }
+}
