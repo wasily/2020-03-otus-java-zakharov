95550dbd461d7eaf44da4c54e5e7bd2a967e7c92
wasily <zaharov_vs@mail.ru>
2020-06-22 11:14:45 +0300

    simple implementation MyCache

--- hw11-cache/src/main/java/ru/otus/hw11/cachehw/MyCache.java
+++ hw11-cache/src/main/java/ru/otus/hw11/cachehw/MyCache.java
@@ -1,33 +1,43 @@
 package ru.otus.hw11.cachehw;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.WeakHashMap;
+
 /**
  * @author sergey
  * created on 14.12.18.
  */
 public class MyCache<K, V> implements HwCache<K, V> {
+    private final WeakHashMap<String, V> cache = new WeakHashMap<>();
+    private final List<HwListener<K, V>> listeners = new ArrayList<>();
 
     @Override
     public void put(K key, V value) {
-
+        cache.put(String.valueOf(key), value);
+        listeners.forEach(x -> x.notify(key, value, "cache put event"));
     }
 
     @Override
     public void remove(K key) {
-
+        V value = cache.remove(String.valueOf(key));
+        listeners.forEach(x -> x.notify(key, value, "cache remove event"));
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V value = cache.get(String.valueOf(key));
+        listeners.forEach(x -> x.notify(key, value, "cache get event"));
+        return value;
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-
+        listeners.add(listener);
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-
+        listeners.remove(listener);
     }
 }
