d13b45207ac51df7d7ff2ff6205388f758e26236
wasily <zaharov_vs@mail.ru>
2020-07-02 13:31:00 +0300

    template imported

---
+++ hw13-ioc/src/main/java/ru/otus/hw13/appcontainer/api/AppComponentsContainer.java
@@ -0,0 +1,6 @@
+package ru.otus.hw13.appcontainer.api;
+
+public interface AppComponentsContainer {
+    <C> C getAppComponent(Class<C> componentClass);
+    <C> C getAppComponent(String componentName);
+}
