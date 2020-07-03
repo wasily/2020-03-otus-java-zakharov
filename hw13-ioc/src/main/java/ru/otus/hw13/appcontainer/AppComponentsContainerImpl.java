cedeafc4a109d09bd5e2b5433e7c7c2e4b63b180
wasily <zaharov_vs@mail.ru>
2020-07-03 11:25:42 +0300

    implementation added

--- hw13-ioc/src/main/java/ru/otus/hw13/appcontainer/AppComponentsContainerImpl.java
+++ hw13-ioc/src/main/java/ru/otus/hw13/appcontainer/AppComponentsContainerImpl.java
@@ -1,12 +1,12 @@
 package ru.otus.hw13.appcontainer;
 
+import ru.otus.hw13.appcontainer.api.AppComponent;
 import ru.otus.hw13.appcontainer.api.AppComponentsContainer;
 import ru.otus.hw13.appcontainer.api.AppComponentsContainerConfig;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
 
@@ -19,7 +19,48 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
-        // You code here...
+        try {
+            var appConfigClassInstance = configClass.getConstructor().newInstance();
+            var methodsStream = Arrays.stream(configClass.getDeclaredMethods()).filter(m -> m.isAnnotationPresent(AppComponent.class));
+            var methodsMap = methodsStream
+                    .collect(Collectors.groupingBy(method -> method.getAnnotation(AppComponent.class).order(),
+                            Collectors.toList()));
+            methodsMap.entrySet().stream()
+                    .sorted(Map.Entry.comparingByKey())
+                    .flatMap(entry -> entry.getValue().stream())
+                    .forEach(method -> {
+                        try {
+                            if (method.getParameterCount() == 0) {
+                                registerBean(method.invoke(appConfigClassInstance),
+                                        method.getAnnotation(AppComponent.class).name());
+                            } else {
+                                Class<?>[] methodParameters = method.getParameterTypes();
+                                List<Object> args = new ArrayList<>(methodParameters.length);
+                                for (Class<?> methodParameter : methodParameters) {
+                                    for (Object appComponent : appComponents) {
+                                        if (methodParameter.isAssignableFrom(appComponent.getClass())) {
+                                            args.add(appComponent);
+                                            break;
+                                        }
+                                    }
+                                }
+                                registerBean(method.invoke(appConfigClassInstance, args.toArray()),
+                                        method.getAnnotation(AppComponent.class).name());
+                            }
+                        } catch (IllegalAccessException | InvocationTargetException e) {
+                            e.printStackTrace();
+                        }
+                    });
+
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void registerBean(Object bean, String name) {
+        appComponents.add(bean);
+        appComponentsByName.put(name, bean);
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -29,12 +70,19 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public <C> C getAppComponent(Class<C> componentClass) {
+        for (Object appComponent : appComponents) {
+            if (componentClass.isAssignableFrom(appComponent.getClass())) {
+                return (C) appComponent;
+            }
+        }
         return null;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) appComponentsByName.get(componentName);
     }
 }
