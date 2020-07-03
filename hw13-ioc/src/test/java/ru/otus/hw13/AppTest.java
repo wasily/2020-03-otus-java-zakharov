cc212000c8a6287c8336d1c3ae4133bf2fcd0720
wasily <zaharov_vs@mail.ru>
2020-07-03 11:33:33 +0300

    test checks added

--- hw13-ioc/src/test/java/ru/otus/hw13/AppTest.java
+++ hw13-ioc/src/test/java/ru/otus/hw13/AppTest.java
@@ -7,8 +7,7 @@ import org.junit.jupiter.api.Test;
 import ru.otus.hw13.appcontainer.AppComponentsContainerImpl;
 import ru.otus.hw13.appcontainer.api.AppComponentsContainer;
 import ru.otus.hw13.config.AppConfig;
-import ru.otus.hw13.services.GameProcessor;
-import ru.otus.hw13.services.GameProcessorImpl;
+import ru.otus.hw13.services.*;
 
 class AppTest {
     private AppComponentsContainer container;
@@ -21,16 +20,25 @@ class AppTest {
     @Test
     void shouldGetBeanByInterface(){
         Assertions.assertNotNull(container.getAppComponent(GameProcessor.class));
+        Assertions.assertNotNull(container.getAppComponent(EquationPreparer.class));
+        Assertions.assertNotNull(container.getAppComponent(IOService.class));
+        Assertions.assertNotNull(container.getAppComponent(PlayerService.class));
     }
 
     @Test
     void shouldGetBeanByImplementation(){
         Assertions.assertNotNull(container.getAppComponent(GameProcessorImpl.class));
+        Assertions.assertNotNull(container.getAppComponent(EquationPreparerImpl.class));
+        Assertions.assertNotNull(container.getAppComponent(IOServiceConsole.class));
+        Assertions.assertNotNull(container.getAppComponent(PlayerServiceImpl.class));
     }
 
     @Test
     void shouldGetBeanByName(){
         Assertions.assertNotNull(container.getAppComponent("gameProcessor"));
+        Assertions.assertNotNull(container.getAppComponent("equationPreparer"));
+        Assertions.assertNotNull(container.getAppComponent("ioService"));
+        Assertions.assertNotNull(container.getAppComponent("playerService"));
     }
 
 }
